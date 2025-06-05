f5b47e040174fd1547bf4c40ec246ce2aa105935
Hetavi Modi <hetavimodi29@gmail.com>
2025-06-05 10:25:39 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <AVLDictionary/AVLDictionary.h>
+
+int main() {
+    AVLDictionary dict;
+    dict.insert("sun", "a star");
+    dict.insert("moon", "a satellite");
+    dict.displayAscending();
+
+    return 0;
+}
